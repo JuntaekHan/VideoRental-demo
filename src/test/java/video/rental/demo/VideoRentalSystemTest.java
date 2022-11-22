352786cd7f1664e3965274e7433cd04acda22165
Jungsun Kim <kimjs@hanyang.ac.kr>
2022-11-23 07:56:14 +0900

    GoldenMaster test platform error fixed

--- src/test/java/video/rental/demo/VideoRentalSystemTest.java
+++ src/test/java/video/rental/demo/VideoRentalSystemTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 public class VideoRentalSystemTest {
 
@@ -22,8 +24,23 @@ public class VideoRentalSystemTest {
 	}
 	
 	@Test
+	@EnabledOnOs(OS.WINDOWS)
 //	@Disabled
-	void check_runResult_against_GoldenMaster() {
+	void check_runResult_against_GoldenMaster_on_Windows() {
+		// Given (Arrange)
+		String expected = goldenMaster.getGoldenMaster();
+		
+		// When (Act)
+		String acutal = goldenMaster.getRunResult();
+
+		// Then (Assert)
+		assertEquals(expected, acutal.replaceAll("\r\n", "\n"));
+	}
+	
+	@Test
+	@EnabledOnOs({OS.MAC, OS.LINUX})
+//	@Disabled
+	void check_runResult_against_GoldenMaster_on_Mac() {
 		// Given (Arrange)
 		String expected = goldenMaster.getGoldenMaster();
 		
