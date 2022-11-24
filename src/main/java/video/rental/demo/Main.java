3e941d788bba6b62731f9fc53ceb916dc2f98a93
Donggon Yoo <stins309@gmail.com>
2022-11-24 19:59:53 +0900

    Graphic UI 사용할 수 있도록 수정함
    Graphic UI 반영

--- src/main/java/video/rental/demo/Main.java
+++ src/main/java/video/rental/demo/Main.java
@@ -1,9 +1,19 @@
 package video.rental.demo;
 
+import video.rental.demo.application.Interactor;
+import video.rental.demo.domain.Repository;
+import video.rental.demo.infrastructure.RepositoryMemImpl;
+import video.rental.demo.presentation.GraphicUI;
+import video.rental.demo.util.SampleGenerator;
+
 public class Main {
 
 	public static void main(String[] args) {
-//		CmdUI ui = new CmdUI();
-//		ui.start();
+		//CmdUI ui = new CmdUI();
+		Repository repository = new RepositoryMemImpl();
+		new SampleGenerator(repository).generateSamples();
+		Interactor interactor = new Interactor(repository);
+		GraphicUI ui = new GraphicUI(interactor);
+		ui.start();
 	}
 }
